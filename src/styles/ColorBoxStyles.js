d8441867c84f8ee9324a002e8cc43d557d16f4c0
zettry1@gmail.com <orgilbat@jrcs.co.jp>
2021-05-08 21:11:24 +0900

    added ereresponsive boxes

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -14,11 +14,11 @@ export default {
     },
     [sizes.down("lg")]: {
       width: "25%",
-      height: (props) => (props.showingFullPalette ? "20%" : "50%"),
+      height: (props) => (props.showingFullPalette ? "20%" : "33.333%"),
     },
     [sizes.down("md")]: {
       width: "50%",
-      height: (props) => (props.showingFullPalette ? "10%" : "50%"),
+      height: (props) => (props.showingFullPalette ? "10%" : "20%"),
     },
     [sizes.down("xs")]: {
       width: "100%",
@@ -124,6 +124,9 @@ export default {
       marginBottom: "0",
       padding: "1rem",
       textTransform: "uppercase",
+      [sizes.down("xs")]: {
+        fontSize: "5rem",
+      },
     },
 
     "& p": {
