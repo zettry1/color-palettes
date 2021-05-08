05f716d41db34fa763064796720d67969ee203e8
zettry1@gmail.com <orgilbat@jrcs.co.jp>
2021-05-08 21:31:09 +0900

    mini platte responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
   root: {
     backgroundColor: "blue",
@@ -12,6 +13,12 @@ export default {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%",
+    },
+    [sizes.down("xs")]: {
+      width: "75%",
+    },
   },
   nav: {
     display: "flex",
@@ -30,6 +37,13 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3,30%)",
-    gridGap: "5%",
+    gridGap: "10%",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2,50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1,100%)",
+      gridGap: "1rem",
+    },
   },
 };
